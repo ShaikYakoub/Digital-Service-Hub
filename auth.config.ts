59c6e2ffdbb6b232d492c6cbfadfbbf976f404db
ShaikYakoub <185510658+ShaikYakoub@users.noreply.github.com>
2025-11-19 14:52:11 +0530

    fix: update OAuth env variable names

--- auth.config.ts
+++ auth.config.ts
@@ -94,8 +94,8 @@ const serverConfig: NextAuthConfig = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
     GitHub({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
     {
       id: "credentials",
