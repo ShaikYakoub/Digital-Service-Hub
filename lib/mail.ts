2e6ca0a3c06f8c7432f11552d08a531d1da3e11f
ShaikYakoub <185510658+ShaikYakoub@users.noreply.github.com>
2025-11-19 20:00:26 +0530

    minor fix - email resend

--- lib/mail.ts
+++ lib/mail.ts
@@ -6,7 +6,7 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `https://digital-service-3sym046r2-shaik-yakoubs-projects.vercel.app/auth/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: "sheikyaqoo@gmail.com", // Change this to your verified domain later
+    from: "onboarding@resend.dev", // Change this to your verified domain later
     to: email,
     subject: "Reset your password",
     html: `<p>Click <a href="${resetLink}">here</a> to reset your password.</p>`
